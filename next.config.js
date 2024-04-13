06e13ad166f0fbe4518a57f7ba82e7ecb9bface2
fipezoGithub <fipezocare@gmail.com>
2024-04-13 11:01:17 +0530

    third commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   assetPrefix: isProd ? "https://rabinsphotography.com" : undefined,
+  images: {
+    remotePatterns: isProd
+      ? [
+          {
+            protocol: "https",
+            hostname: "rabinsphotography.com",
+            pathname: "**",
+          },
+        ]
+      : undefined,
+  },
 };
 
 module.exports = nextConfig;
